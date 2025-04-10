2996a610ed4a0daf0d4d87a0c7df98b5910164e6
julioventura <julioventura@gmail.com>
2025-04-10 03:04:36 -0300

    ajuste

--- js/player.js
+++ js/player.js
@@ -12,12 +12,14 @@ class Player {
         this.width = 40;    // Largura do canhão
         this.height = 48;   // DOBRADO - era 24 pixels
         this.speed = 10;    // Velocidade de movimentação
-        this.maxProjectiles = 2;
-        this.shooting = false;
         this.lives = 2;
         this.isBlinking = false;
         this.blinkTimer = 0;
         this.blinkInterval = 200;
+        this.shooting = false;
+        
+        // O número máximo de tiros é igual ao número de vidas
+        this.maxProjectiles = this.lives;
 
         // Sprite do canhão (16x16) - mantém o mesmo design, mas será esticado verticalmente
         this.sprite = [
@@ -78,6 +80,10 @@ class Player {
 
     loseLife() {
         this.lives--;
+        
+        // Atualiza o número máximo de tiros com base nas vidas restantes
+        this.maxProjectiles = this.lives; // Com 1 vida, só pode ter 1 tiro
+        
         if (this.lives > 0) {
             // Se ainda tem vida, começa a piscar
             this.isBlinking = true;
